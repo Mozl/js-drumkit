e9d1c0b3391db54109f142a53e18c501bd0de659
Louis Moselhi <moselhil@gmail.com>
2018-06-16 19:38:50 +0100

    tests keyboard press for g

--- cypress/integration/drumkit.spec.js
+++ cypress/integration/drumkit.spec.js
@@ -1,7 +1,7 @@
 describe('My First Test', function() {
   it('Does not do much!', function() {
     expect(true).to.equal(true);
-  })
+  });
 
   it('visits the drumkit page', function() {
     cy.visit('http://127.0.0.1:5500/index.html');
@@ -10,5 +10,10 @@ describe('My First Test', function() {
   it('finds Tom element', function() {
     cy.visit('http://127.0.0.1:5500/index.html');
     cy.contains('boom');
+  });
+
+  it('plays a sound', function() {
+    cy.visit('http://127.0.0.1:5500/index.html');
+    cy.get('#g').type('g');
   });
 });
