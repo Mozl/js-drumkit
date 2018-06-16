91dae7c4e2698dc4207e083cb18b3c272cf70a66
Louis Moselhi <moselhil@gmail.com>
2018-06-16 18:30:28 +0100

    test for page visit

--- cypress/integration/drumkit.spec.js
+++ cypress/integration/drumkit.spec.js
@@ -1,5 +1,9 @@
 describe('My First Test', function() {
   it('Does not do much!', function() {
-    expect(true).to.equal(true)
+    expect(true).to.equal(true);
   })
-})
+
+  it('visits the drumkit page', function() {
+    cy.visit('http://127.0.0.1:5500/index.html');
+  });
+});
